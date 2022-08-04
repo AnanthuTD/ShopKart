239e7b690f0423307c5e8d1d3123cab3b6763ce0
AnanthuTD <ananthutd2021@gmail.com>
2022-08-04 20:28:48 +0530

    beginning user-header & user-header

---
+++ public/javascripts/script.js
@@ -0,0 +1,10 @@
+
+console.log("hi");
+$(document).ready(function(){
+    $(".el, .tv, .men, .women, .kids, .sports, .home").hover( function() {
+            $(".catagory_dropdown").css({ "display": "block" })
+        },
+        function(){
+            $(".catagory_dropdown").css({ "display": "none" })
+        })
+  })
