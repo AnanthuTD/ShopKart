d2d1bc101568b0ff06f7b8a118e9b030e0e199d0
AnanthuTD <ananthutd2021@gmail.com>
2022-11-08 20:20:16 +0530

    DB changed to cloud

---
+++ helpers/config-helpers.js
@@ -0,0 +1,37 @@
+const collections = require('../config/collections');
+let promise = require('promise');
+
+module.exports.createIndex = (db) => {
+    createIndex(db);
+}
+function createIndex(db) {
+    return new promise((resolve, reject) => {
+
+        db.collection(collections.PRODUCT_COLLECTION).dropIndex("productIndex").catch(err => {
+            if (err.codeName == 'NamespaceNotFound') {
+                db.createCollection(collections.PRODUCT_COLLECTION).catch(err => {
+                    console.log("FAILD TO CREATE " + collections.PRODUCT_COLLECTION);
+                }).then((res) => {
+                    console.log(res);
+                })
+            }
+        }).then(() => {
+            db.collection(collections.PRODUCT_COLLECTION).createIndex(
+                {
+                    name: "text",
+                    index: "text",
+
+                },
+                {
+                    weights: {
+                        index: 2,
+                    },
+                    name: "productIndex"
+                }
+            ).catch((err) => {
+                resolve()
+            })
+            resolve()
+        })
+    })
+}
