6c1facff21c69aa4adc1c677c5027621d7f7617f
AnanthuTD <ananthutd2021@gmail.com>
2023-01-29 18:40:09 +0530

    bug fix

--- config/session.js
+++ config/session.js
@@ -2,14 +2,15 @@ var session = require('express-session')
 var MongoStore = require('connect-mongo')
 
 
-module.exports = async(app) => {
-    let uri = process.env.DB_URI;
+module.exports = async (app) => {
+    // let uri = process.env.DB_URI;
+    let uri = process.env.LOCAL_DB
+   
     app.use(session({
         secret: 'key', cookie: { maxAge: 1 * 60 * 60 * 1000 }, // = 1hour //hh:mm:ss:millisec
         resave: false, saveUninitialized: false,
         store: MongoStore.create({
-        mongoUrl: uri
-
+           mongoUrl: uri
         })
     }));
 }
